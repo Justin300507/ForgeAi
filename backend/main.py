e3b9293d43049deb8833165d797cc2bc7b2e577c
Justin Thomas <booombam530@gmail.com>
2026-06-14 21:45:05 +0530

    ForgeAI connected to Gemini

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from google import genai
 
 app = FastAPI()
+client = genai.Client(
+    api_key="AQ.Ab8RN6IIS4CGBRYgzHs_UVN4cFxDUE-zrm1xi9JrudCx5meFbw"
+)
+
+class ProjectIdea(BaseModel):
+    idea: str
 
 @app.get("/")
 def home():
     return {"message": "ForgeAI Running"}
 
 @app.post("/generate")
-def generate():
+def generate(project: ProjectIdea):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"""
+        Create a software project plan.
+
+        Idea:
+        {project.idea}
+
+        Include:
+        - Project Name
+        - Features
+        - Tech Stack
+        - Roadmap
+        """
+    )
+
     return {
-        "project_name": "Gym Tracker",
-        "features": [
-            "Authentication",
-            "Workout Logging",
-            "Progress Tracking"
-        ],
-        "tech_stack": [
-            "FastAPI",
-            "React",
-            "PostgreSQL"
-        ]
-    }
+        "idea": project.idea,
+        "plan": response.text}
